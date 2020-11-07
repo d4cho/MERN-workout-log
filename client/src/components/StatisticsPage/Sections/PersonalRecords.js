322369d51b3a741aca382da1fc02064fdc59d919
Daniel Cho <danielcho91@gmail.com>
2020-11-07 13:23:17 -0500

    render personal records

--- client/src/components/StatisticsPage/Sections/PersonalRecords.js
+++ client/src/components/StatisticsPage/Sections/PersonalRecords.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import { Table } from 'reactstrap';
 
-const PersonalRecords = () => {
+const X_REP_MAX = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+
+const PersonalRecords = (props) => {
+  const repMaxCalculator = (rep, data) => {
+    let max = 0;
+    for (const session of data) {
+      for (const set of session.info) {
+        if (set.reps === rep && set.weight > max) {
+          max = set.weight;
+        }
+      }
+    }
+    return max;
+  };
+
+  const renderTableBody = X_REP_MAX.map((rep) => (
+    <tr key={rep}>
+      <th scope='row'>{rep} Reps</th>
+      <td>{repMaxCalculator(rep, props.exerciseStats)} lbs</td>
+    </tr>
+  ));
+
   return (
     <Table>
       <thead>
@@ -11,7 +32,8 @@ const PersonalRecords = () => {
         </tr>
       </thead>
       <tbody>
-        <tr>
+        {renderTableBody}
+        {/* <tr>
           <th scope='row'>1 Rep</th>
           <td>100.00 lbs</td>
         </tr>
@@ -22,7 +44,7 @@ const PersonalRecords = () => {
         <tr>
           <th scope='row'>3 Reps</th>
           <td>80.00 lbs</td>
-        </tr>
+        </tr> */}
       </tbody>
     </Table>
   );
