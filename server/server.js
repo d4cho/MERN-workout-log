d8a2af76b92ff8ec799c4aefb90661c00bd016ad
Daniel Cho <danielcho91@gmail.com>
2020-11-10 18:25:04 -0500

    deploy to heroku

--- server/server.js
+++ server/server.js
@@ -33,6 +33,19 @@ app.use('/api/notifications', require('./routes/notifications'));
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
 
+// for deployment
+// server static assets if in production mode
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  // all js and css files will be read from build folder
+  app.use(express.static('client/build'));
+
+  // index.html for all page routes
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
+
 // Use port
 const port = process.env.PORT || 5000;
 
